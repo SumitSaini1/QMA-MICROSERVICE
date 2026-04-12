8b1c37260ac5ccad32f4a8ea36ca372dcdca2012
SumitSaini1 <sumitsaini21027@gmail.com>
2026-04-12 16:24:02 +0530

    [Sumit Saini] fix: properties

--- auth-service/src/main/java/com/quantity/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/quantity/auth_service/AuthServiceApplication.java
@@ -2,21 +2,21 @@ package com.quantity.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Auth Service API", version = "v1"))
+
 public class AuthServiceApplication {
 
-    public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-            .ignoreIfMissing()
-            .load();
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+        .ignoreIfMissing()
+        .load();
 
-        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID", ""));
-        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET", ""));
-        SpringApplication.run(AuthServiceApplication.class, args);
-    }
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
 }
--- quantity-service/src/main/java/com/quantity/quantity_service/QuantityServiceApplication.java
+++ quantity-service/src/main/java/com/quantity/quantity_service/QuantityServiceApplication.java
@@ -2,14 +2,12 @@ package com.quantity.quantity_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Quantity Service API", version = "v1"))
 public class QuantityServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(QuantityServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(QuantityServiceApplication.class, args);
+	}
+
 }

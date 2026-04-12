03e74b33a69740da4f9bf979ed8bceb9d9669724
SumitSaini1 <sumitsaini21027@gmail.com>
2026-04-12 14:10:08 +0530

    [Sumit Saini] fix: google url

--- auth-service/src/main/java/com/quantity/auth_service/security/OAuth2SuccessHandler.java
+++ auth-service/src/main/java/com/quantity/auth_service/security/OAuth2SuccessHandler.java
@@ -1,20 +1,25 @@
 package com.quantity.auth_service.security;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
-import com.quantity.auth_service.dto.LoginResponseDto;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import com.quantity.auth_service.dto.LoginResponseDto;
 import com.quantity.auth_service.service.AuthService;
-import org.springframework.context.annotation.Lazy;
-import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
@@ -42,7 +47,8 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         String jwt = loginResponse.getJwt();;
 
         // String redirectUrl = "http://localhost:5501/index.html?token=" + jwt;
-        String redirectUrl = "http://localhost:4200/login?token=" + jwt;
+        String redirectUrl = "https://qmamicroservice.netlify.app/login?token=" 
+        + URLEncoder.encode(jwt, StandardCharsets.UTF_8);
 
         response.sendRedirect(redirectUrl);
     }
